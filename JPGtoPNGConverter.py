9f4f73c47dab3b729dd0397fcccff2bc783e1b1d
VagishM6 <89293509+VagishM6@users.noreply.github.com>
2021-08-23 07:38:54 +0530

    Update JPGtoPNGConverter.py

--- JPGtoPNGConverter.py
+++ JPGtoPNGConverter.py
@@ -3,15 +3,15 @@ import os
 from PIL import Image, ImageFilter
 
 
-# grab the first and second args
+# grab the first and second args from the user
 image_folder = sys.argv[1]
 output_folder = sys.argv[2]
 
-# check if new exist/ if not create it
+# check if (new) folder exist/ if not create it
 if not os.path.exists(output_folder):
     os.makedirs(output_folder)
 
-# loop through pokedex, then convert images to png
+# loop through the directory, then convert (image.type) to png
 for filename in os.listdir(image_folder):
     img = Image.open(f'{image_folder}{filename}')
     clean_name = os.path.splitext(filename)[0]
